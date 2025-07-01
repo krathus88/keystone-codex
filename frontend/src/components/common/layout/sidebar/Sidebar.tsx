dede1c721f29e42499e1aa4ed35d742a47a8da26
Krathus88 <marcoribeiro4@hotmail.com>
2025-07-01 18:07:45 +0100

    Clean up Sidebar

--- frontend/src/components/common/layout/sidebar/Sidebar.tsx
+++ frontend/src/components/common/layout/sidebar/Sidebar.tsx
@@ -1,43 +1,10 @@
-import { Link as ChakraLink, Flex, Image } from "@chakra-ui/react"
-import { CURRENT_SEASON_DUNGEONS } from "@models/Map"
-import { MapService } from "@services/MapService"
-import { Link as TanstackLink } from "@tanstack/react-router"
-import { Tooltip } from "@ui/tooltip"
+import { Flex } from "@chakra-ui/react"
+import { SeasonDungeonList } from "./SeasonDungeonList"
 
 export const Sidebar = () => {
     return (
         <Flex p={2} flexDirection={"column"}>
-            <Flex flexDirection={"column"} gap={1}>
-                {CURRENT_SEASON_DUNGEONS.map((map, index) => {
-                    const iconUrl = MapService.getCurrentSeasonMapIcon(
-                        map.value,
-                    )
-
-                    if (!iconUrl) return null
-
-                    return (
-                        <ChakraLink key={index} w="fit-content" asChild>
-                            <TanstackLink
-                                to="/map/$map"
-                                params={{ map: map.value }}
-                            >
-                                <Tooltip
-                                    content={map.label}
-                                    positioning={{ placement: "right" }}
-                                >
-                                    <Image
-                                        src={iconUrl}
-                                        w={"50px"}
-                                        h={"50px"}
-                                        borderRadius={"md"}
-                                        _hover={{ opacity: 0.8 }}
-                                    />
-                                </Tooltip>
-                            </TanstackLink>
-                        </ChakraLink>
-                    )
-                })}
-            </Flex>
+            <SeasonDungeonList />
         </Flex>
     )
 }
