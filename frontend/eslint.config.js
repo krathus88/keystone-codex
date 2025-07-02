bc354bfdc4c1c90e4069392cb0a22d2886537489
Krathus88 <marcoribeiro4@hotmail.com>
2025-07-02 09:18:24 +0100

    Update eslint config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,35 +1,30 @@
-import js from "@eslint/js"
-import globals from "globals"
+import eslint from "@eslint/js"
+import eslintConfigPrettierRecommended from "eslint-config-prettier/recommended"
 import reactHooks from "eslint-plugin-react-hooks"
 import reactRefresh from "eslint-plugin-react-refresh"
+import globals from "globals"
 import tseslint from "typescript-eslint"
-import eslintPluginPrettier from "eslint-plugin-prettier"
 
 export default tseslint.config(
     { ignores: ["dist"] },
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    eslintConfigPrettierRecommended,
+    reactHooks.configs["recommended-latest"],
+    reactRefresh.configs.recommended,
     {
-        extends: [
-            js.configs.recommended,
-            ...tseslint.configs.recommended,
-            eslintPluginPrettier.configs.recommended,
-            prettier,
-        ],
         files: ["**/*.{ts,tsx}"],
         languageOptions: {
             ecmaVersion: 2020,
             globals: globals.browser,
         },
-        plugins: {
-            "react-hooks": reactHooks,
-            "react-refresh": reactRefresh,
-        },
         rules: {
-            ...reactHooks.configs.recommended.rules,
             "react-hooks/react-compiler": "error",
             "react-refresh/only-export-components": [
                 "warn",
                 { allowConstantExport: true },
             ],
+            "prettier/prettier": "error",
         },
     },
 )
